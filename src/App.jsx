58a730f2ef6e516e124c64876e496aafc7ab04f7
Ahmed Reza <ahmedrezashams4@gmail.com>
2025-04-06 09:39:39 +0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function App() {
+    return (
+     <div>
+           <h1 className="text-5xl font-bold underline text-red-500">
+    Hello world!
+  </h1>
+     </div>
+    )
+}
+
+export default App
